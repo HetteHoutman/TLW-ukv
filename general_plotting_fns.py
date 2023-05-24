3af4f235a3aaf46ec364907fe9c49158d8e15cba
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-05-24 15:32:25 +0100

    cleaned

--- general_plotting_fns.py
+++ general_plotting_fns.py
@@ -1,7 +1,18 @@
 import matplotlib.colors as colors
 
 def centred_cnorm(data):
-    w_min = data.min()
-    w_max = data.max()
-    range_max = max(abs(w_max), abs(w_min))
+    """
+    use for "norm" kwarg in plt for a colormap normalisation that is centred around zero.
+    Parameters
+    ----------
+    data : ndarray
+        the data that is plotted
+
+    Returns
+    -------
+    colors.Normalize
+    """
+    data_min = data.min()
+    data_max = data.max()
+    range_max = max(abs(data_max), abs(data_min))
     return colors.Normalize(vmin=-range_max, vmax=range_max)
