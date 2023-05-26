65a67b704b410f7ee3139caa956ccee3dba5ed3f
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-05-26 10:56:32 +0100

    docstring and more general functions

--- miscellaneous.py
+++ miscellaneous.py
@@ -1,8 +1,6 @@
 import numpy as np
-from pyproj import Geod
 from iris.analysis import trajectory
-
-from plot_profile_from_UKV import convert_to_ukv_coords
+from pyproj import Geod
 
 
 def make_great_circle_points(start, end, n):
@@ -26,12 +24,24 @@ def make_great_circle_points(start, end, n):
     great_circle = np.array(g.npts(*start, *end, n, initial_idx=0, terminus_idx=0)).T
     return great_circle
 
-def make_great_circle_traj(gc, n):
-    waypoints = [{'grid_longitude': gc[0][0], 'grid_latitude': gc[0][1]},
-                 {'grid_longitude': gc[-1][0], 'grid_latitude': gc[-1][1]}]
-    traj = trajectory.Trajectory(waypoints, sample_count=n)
+def make_custom_traj(sample_points):
+    """
+    Returns an iris.analysis.Trajectory instance with sample points given by gc
+    Parameters
+    ----------
+    sample_points : ndarray
+        ndarray of shape (m, 2) containing the sample points for the trajectory instance.
+        should be in format lon/lat
+
+    Returns
+    -------
+    iris.analysis.Trajectory
+    """
+    waypoints = [{'grid_longitude': sample_points[0][0], 'grid_latitude': sample_points[0][1]},
+                 {'grid_longitude': sample_points[-1][0], 'grid_latitude': sample_points[-1][1]}]
+    traj = trajectory.Trajectory(waypoints, sample_count=sample_points.shape[0])
     # replace trajectory points which are equally spaced in lat/lon with great circle points
-    for gcpoint, d in zip(gc, traj.sampled_points):
+    for gcpoint, d in zip(sample_points, traj.sampled_points):
         d['grid_longitude'] = gcpoint[0]
         d['grid_latitude'] = gcpoint[1]
 
