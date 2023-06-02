c275c9c9ae6f85dcfa45c345d093ef55ae3a53a1
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-06-02 15:45:36 +0100

    absolute savefile locations

--- plot_crosssection.py
+++ plot_crosssection.py
@@ -65,8 +65,8 @@ def plot_xsect_map(cube_single_level, great_circle=None, cmap="brewer_PuOr_11", 
               f'm {year}/{month}/{day} at {s.h}h ({forecast_time})')
 
     plt.tight_layout()
-    my_path = os.path.abspath('plots/')
-    plt.savefig(os.path.join(my_path, f'xsect_map{custom_save}_{year}{month}{day}_{s.h}.png'), dpi=300)
+    my_dir = os.path.dirname(os.path.abspath(__file__))
+    plt.savefig(os.path.join(my_dir, f'plots/xsect_map{custom_save}_{year}{month}{day}_{s.h}.png'), dpi=300)
     plt.show()
 
 
@@ -113,8 +113,8 @@ def plot_xsect(w_xsect, theta_xsect, RH_xsect, max_height=5000, cmap="brewer_PuO
     plt.ylim((0, max_height))
     plt.xlabel(f'Distance along great circle / {w_xsect.coord(coords[0]).units}')
 
-    my_path = os.path.abspath('plots/')
-    plt.savefig(os.path.join(my_path, f'xsect{custom_save}_{year}{month}{day}_{s.h}.png'), dpi=300)
+    my_dir = os.path.dirname(os.path.abspath(__file__))
+    plt.savefig(os.path.join(my_dir, f'plots/xsect{custom_save}_{year}{month}{day}_{s.h}.png'), dpi=300)
     plt.show()
 
 
