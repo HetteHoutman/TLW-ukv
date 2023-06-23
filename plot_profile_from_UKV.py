06349b9f55d7f4661587072a8c7b2c2324f8dd4e
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-06-23 12:19:06 +0100

    now should plot altitude instead of height but not really tested. needs to be cleaned up too

--- plot_profile_from_UKV.py
+++ plot_profile_from_UKV.py
@@ -1,11 +1,12 @@
 import sys
 
 import cartopy.crs as ccrs
+import iris
 import matplotlib.pyplot as plt
 import numpy as np
 
 import thermodynamics as th
-from cube_processing import read_variable
+from cube_processing import read_variable, check_level_heights, add_orography
 from met_fns import uv_to_spddir, N_squared, scorer_param
 from miscellaneous import convert_to_ukv_coords, index_selector, load_settings
 from plot_profile_from_txt import plot_profile
@@ -29,13 +30,14 @@ if __name__ == '__main__':
     p_theta_cube = read_variable(filename, 408, h)
     T_cube = read_variable(filename, 16004, h)
     q_cube = read_variable(filename, 10, h)
+    orog_cube = read_variable(s.orog_file, 33, s.orog_h)
 
-    # only plot certain heights
-    min_height = 20
-    max_height = 5000
-    height = u_cube.coord('level_height').points
-    level_mask = (height < max_height) & (height > min_height)
-    height = height[level_mask]
+    q_cube = check_level_heights(q_cube, T_cube)
+
+    u_cube = u_cube.regrid(T_cube, iris.analysis.Linear())
+    v_cube = v_cube.regrid(T_cube, iris.analysis.Linear())
+
+    u_cube, v_cube, p_theta_cube, T_cube, q_cube = add_orography(orog_cube, u_cube, v_cube, p_theta_cube, T_cube, q_cube)
 
     # coordinates given in regular lat lon, convert to model's rotated pole system
     # currently the code just plots the profiles at the nearest T grid point of the model.
@@ -58,30 +60,34 @@ if __name__ == '__main__':
     true_model_y = lats[lat_index]
 
     # calculate theta
-    theta_col = th.potential_temperature(T_cube.data[level_mask, lat_index, lon_index],
-                                         p_theta_cube.data[level_mask, lat_index, lon_index])
+    theta_col = th.potential_temperature(T_cube.data[:, lat_index, lon_index],
+                                         p_theta_cube.data[:, lat_index, lon_index])
 
-    # interpolate winds onto T grid points (Arakawa C-grid) and convert back to latlon from ukv rotated pole
-    u_col = 0.5*(u_cube.data[level_mask, lat_index, lon_index-1] +
-                 u_cube.data[level_mask, lat_index, lon_index])
-    v_col = 0.5*(v_cube.data[level_mask, lat_index-1, lon_index] +
-                 v_cube.data[level_mask, lat_index, lon_index])
-    u_latlon, v_latlon = crs_latlon.transform_vectors(crs_rotated, np.full_like(u_col, true_model_x),
-                                                      np.full_like(u_col, true_model_y), u_col, v_col)
+    u_col = u_cube[:, lat_index, lon_index]
+    v_col = v_cube[:, lat_index, lon_index]
+
+    u_latlon, v_latlon = crs_latlon.transform_vectors(crs_rotated, np.full_like(u_col.data, true_model_x),
+                                                      np.full_like(u_col.data, true_model_y), u_col.data, v_col.data)
     spd_col, dir_col = uv_to_spddir(u_latlon, v_latlon)
 
+    # only plot certain heights
+    min_height = 20
+    max_height = 5000
+    height = u_col.coord('altitude').points
+    level_mask = (height < max_height) & (height > min_height)
+    height = height[level_mask]
+
     # N squared
-    N2 = N_squared(theta_col, height)
-    N2U2 = N2 / u_col ** 2
-    l2 = scorer_param(N2, u_col, height)
+    N2 = N_squared(theta_col[level_mask], height)
+    N2U2 = N2 / u_col.data[level_mask] ** 2
+    l2 = scorer_param(N2, u_col.data[level_mask], height)
 
     # plot
-    fig = plot_profile(l2, height, N2U2, theta_col, spd_col, dir_col, figsize=(8,4))
+    fig = plot_profile(l2, height, N2U2, theta_col[level_mask], spd_col[level_mask], dir_col[level_mask], figsize=(8,4))
 
     true_x, true_y = crs_latlon.transform_point(true_model_x, true_model_y, crs_rotated)
     title = f'UKV ({true_x:.02f}, {true_y:.02f}) on {year}/{month}/{day} at {h} ({forecast_time})'
     # plt.suptitle(title)
     plt.tight_layout()
     plt.savefig(f'plots/profile_from_UKV_({true_x:.02f}_{true_y:.02f})_{year}{month}{day}_{h}.png', dpi=300)
-    print(N2)
     plt.show()
