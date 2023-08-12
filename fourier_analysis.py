e6bf9ec37dad1ce6e20eb7afff9e7aab6c9324bc
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-08-12 14:37:06 +0200

    better polar pspec plot

--- fourier_analysis.py
+++ fourier_analysis.py
@@ -150,8 +150,13 @@ def plot_interp_pcolormesh(wnum_bins_interp, theta_bins_interp, interp_values):
     plt.show()
 
 
-def plot_pspec_polar(wnum_bins, theta_bins, radial_pspec_array, scale='linear', xlim=None):
-    plt.pcolormesh(wnum_bins, theta_bins, radial_pspec_array, norm=mpl.colors.LogNorm())
+def plot_pspec_polar(wnum_bins, theta_bins, radial_pspec_array, scale='linear', xlim=None, vmin=None, vmax=None):
+    if vmin is not None and vmax is not None:
+        plt.pcolormesh(wnum_bins, theta_bins, radial_pspec_array, norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax))
+        plt.colorbar(extend='both')
+    else:
+        plt.pcolormesh(wnum_bins, theta_bins, radial_pspec_array, norm=mpl.colors.LogNorm())
+        plt.colorbar()
     plt.xscale(scale)
     if xlim is not None:
         plt.xlim(xlim)
@@ -163,7 +168,6 @@ def plot_pspec_polar(wnum_bins, theta_bins, radial_pspec_array, scale='linear', 
     plt.colorbar()
     plt.xlabel(r"$|\mathbf{k}|$" + ' / ' + r"$\rm{km}^{-1}$")
     plt.ylabel(r'$\theta$')
-
 
 def plot_wind(w_cube, u_cube, v_cube, step=25):
     fig, ax = plt.subplots(1, 1,
@@ -250,10 +254,11 @@ if __name__ == '__main__':
     dominant_wnum, dominant_theta = find_max(bounded_polar_pspec, bounded_wnum_vals, theta_vals)
 
     plot_pspec_polar(wnum_bins, theta_bins, radial_pspec)
-
     plt.scatter(dominant_wnum, dominant_theta, marker='x', color='k', s=100, zorder=100)
     plt.show()
-    plot_pspec_polar(wnum_bins, theta_bins, radial_pspec, scale='log', xlim=(0.05, 4.5))
+
+    plot_pspec_polar(wnum_bins, theta_bins, radial_pspec, scale='log', xlim=(0.05, 4.5), vmin=bounded_polar_pspec.min(),
+                     vmax=bounded_polar_pspec.max())
     plt.scatter(dominant_wnum, dominant_theta, marker='x', color='k', s=100, zorder=100)
     plt.savefig('plots/' + str(sys.argv[1][9:-5]) + '/polar_pspec.png', dpi=300)
     plt.show()
