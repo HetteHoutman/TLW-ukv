60864c17e555f8329366ea25b193fb3289b925c1
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-08-15 18:46:28 +0200

    fourier plot lib and gitignore

--- fourier_analysis.py
+++ fourier_analysis.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 import cartopy.crs as ccrs
@@ -6,167 +5,16 @@ import iris.cube
 import iris.plot as iplt
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
+import os
 from iris.analysis.cartography import rotate_winds
-from matplotlib import ticker, colors
+from matplotlib import colors
 
 from cube_processing import read_variable, cube_at_single_level, add_orography, add_true_latlon_coords, \
     create_latlon_cube
 from fourier import *
+from fourier_plot import plot_pspec_polar, plot_radial_pspec, plot_2D_pspec, filtered_inv_plot
 from miscellaneous import check_argv_num, load_settings, get_bounds
 
-
-def filtered_inv_plot(img, filtered_ft, Lx, Ly, latlon=None, inverse_fft=True):
-    if inverse_fft:
-        fig, (ax1, ax3) = plt.subplots(1, 2, sharey=True)
-    else:
-        fig, ax1 = plt.subplots(1, 1)
-
-    xlen = img.shape[1]
-    ylen = img.shape[0]
-
-    if latlon:
-        physical_extent = [latlon[0], latlon[2], latlon[1], latlon[3]]
-        xlabel = 'Longitude'
-        ylabel = 'Latitude'
-    else:
-        pixel_x = Lx / xlen
-        pixel_y = Ly / ylen
-        physical_extent = [-Lx / 2 - pixel_x / 2, Lx / 2 + pixel_x / 2, -Ly / 2 - pixel_y / 2, Ly / 2 + pixel_y / 2]
-        xlabel = 'x distance / km'
-        ylabel = 'y distance / km'
-
-    ax1.imshow(img,
-               extent=physical_extent,
-               cmap='gray')
-    ax1.set_ylabel(ylabel)
-    ax1.set_xlabel(xlabel)
-
-    if inverse_fft:
-        inv = np.fft.ifft2(filtered_ft.filled(fill_value=1))
-        ax3.set_title(f'{min_lambda} km < lambda < {max_lambda} km')
-        ax3.imshow(abs(inv),
-                   extent=physical_extent,
-                   cmap='gray')
-    # save?
-    plt.tight_layout()
-    plt.savefig(save_path + '/sat_plot.png', dpi=300)
-    plt.show()
-
-
-def plot_2D_pspec(bandpassed_pspec, Lx, Ly, wavelength_contours=None):
-    xlen = bandpassed_pspec.shape[1]
-    ylen = bandpassed_pspec.shape[0]
-
-    fig2, ax2 = plt.subplots(1, 1)
-    # TODO change to pcolormesh? might be useful in search for maximum
-    max_k = xlen // 2 * 2 * np.pi / Lx
-    max_l = ylen // 2 * 2 * np.pi / Ly
-    pixel_k = 2 * max_k / xlen
-    pixel_l = 2 * max_l / ylen
-    recip_extent = [-max_k - pixel_k / 2, max_k + pixel_k / 2, -max_l - pixel_l / 2, max_l + pixel_l / 2]
-
-    im = ax2.imshow(bandpassed_pspec.data, extent=recip_extent, interpolation='none',
-                    norm=mpl.colors.LogNorm(vmin=bandpassed_pspec.min(), vmax=bandpassed_pspec.max()))
-
-    if wavelength_contours:
-        K, L, dist_array, thetas = recip_space(Lx, Ly, bandpassed_pspec.shape)
-        wavelengths = 2 * np.pi / dist_array
-        con = ax2.contour(K, L, wavelengths, levels=wavelength_contours, colors=['k'], linestyles=['--'])
-        ax2.clabel(con)
-
-    ax2.set_title('2D Power Spectrum')
-    ax2.set_xlabel(r"$k_x$" + ' / ' + r"$\rm{km}^{-1}$")
-    ax2.set_ylabel(r"$k_y$" + ' / ' + r"$\rm{km}^{-1}$")
-    ax2.set_xlim(-2, 2)
-    ax2.set_ylim(-2, 2)
-    fig2.colorbar(im, extend='both')
-    plt.tight_layout()
-    plt.savefig(save_path + '/2d_pspec.png', dpi=300)
-    plt.show()
-
-
-def plot_radial_pspec(pspec_array, vals, theta_ranges, dom_wnum):
-    plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.rainbow(np.linspace(0, 1, len(pspec_array))))
-    for i, pspec in enumerate(pspec_array):
-        plt.loglog(vals, pspec, label=f'{theta_ranges[i]}' + r'$ \leq \theta < $' + f'{theta_ranges[i + 1]}')
-
-    xlen = orig.shape[1]
-    ylen = orig.shape[0]
-    pixel_x = Lx / xlen
-    pixel_y = Ly / ylen
-    ymin = np.nanmin(np.array(pspec_array))
-    ymax = np.nanmax(np.array(pspec_array))
-
-    plt.vlines(2 * np.pi / 8, ymin, ymax, 'k', linestyles='--')
-    # plt.vlines(2 * np.pi / min(Lx, Ly), ymin, ymax, 'k', linestyles='dotted')
-    # plt.vlines(np.pi / max(pixel_y, pixel_x), ymin, ymax, 'k', linestyles='dotted')
-    plt.vlines(2 * np.pi / min_lambda, ymin, ymax, 'k', linestyles='-.')
-    plt.vlines(2 * np.pi / max_lambda, ymin, ymax, 'k', linestyles='-.')
-    plt.vlines(dom_wnum, ymin, ymax, 'k')
-
-    plt.title('1D Power Spectrum')
-    plt.xlabel(r"$|\mathbf{k}|$" + ' / ' + r"$\rm{km}^{-1}$")
-    plt.ylabel(r"$P(|\mathbf{k}|)$")
-    plt.ylim(ymin, ymax)
-    plt.legend(loc='lower left')
-    plt.tight_layout()
-    plt.savefig(save_path + '/radial_pspec.png', dpi=300)
-    plt.show()
-
-
-def plot_ang_pspec(pspec_array, vals, wavelength_ranges):
-    plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.rainbow(np.linspace(0, 1, len(pspec_array))))
-    for i, pspec in enumerate(pspec_array):
-        plt.plot(vals, pspec,
-                 label=f'{wavelength_ranges[i]} km' + r'$ \leq \lambda < $' + f'{wavelength_ranges[i + 1]} km')
-
-    ax = plt.gca()
-    ax.set_yscale('log')
-    plt.title('Angular power spectrum')
-    plt.ylabel(r"$P(\theta)$")
-    plt.xlabel(r'$\theta$ (deg)')
-    plt.legend(loc='lower left')
-    plt.grid()
-    plt.show()
-
-
-def plot_interp_contour(grid, interp_values):
-    """Plots interpolated values on a contour plot. Colourscale is weird though"""
-    con = plt.contourf(grid[:, :, 0], grid[:, :, 1], interp_values, locator=ticker.LogLocator())
-    plt.colorbar(con, extend='both')
-    plt.xlabel(r"$|\mathbf{k}|$" + ' / ' + r"$\rm{km}^{-1}$")
-    plt.ylabel(r'$\theta$')
-    plt.show()
-
-
-def plot_interp_pcolormesh(wnum_bins_interp, theta_bins_interp, interp_values):
-    """Plots interpolated values on a pcolormesh plot."""
-    plt.pcolormesh(wnum_bins_interp, theta_bins_interp, interp_values,
-                   norm=colors.LogNorm(vmin=pspec_2d.min(), vmax=pspec_2d.max()), )
-    plt.colorbar(extend='both')
-    plt.xlabel(r"$|\mathbf{k}|$" + ' / ' + r"$\rm{km}^{-1}$")
-    plt.ylabel(r'$\theta$')
-    plt.show()
-
-
-def plot_pspec_polar(wnum_bins, theta_bins, radial_pspec_array, scale='linear', xlim=None, vmin=None, vmax=None):
-    if vmin is not None and vmax is not None:
-        plt.pcolormesh(wnum_bins, theta_bins, radial_pspec_array, norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax))
-        plt.colorbar(extend='both')
-    else:
-        plt.pcolormesh(wnum_bins, theta_bins, radial_pspec_array, norm=mpl.colors.LogNorm())
-        plt.colorbar()
-    plt.xscale(scale)
-    if xlim is not None:
-        plt.xlim(xlim)
-    plt.ylim(theta_bins[0], theta_bins[-1])
-
-    plt.vlines(2 * np.pi / min_lambda, theta_bins[0], theta_bins[-1], 'k', linestyles='-.')
-    plt.vlines(2 * np.pi / max_lambda, theta_bins[0], theta_bins[-1], 'k', linestyles='-.')
-
-    plt.xlabel(r"$|\mathbf{k}|$" + ' / ' + r"$\rm{km}^{-1}$")
-    plt.ylabel(r'$\theta$')
 
 def plot_wind(w_cube, u_cube, v_cube, step=25):
     fig, ax = plt.subplots(1, 1,
@@ -240,10 +88,14 @@ if __name__ == '__main__':
     filtered_inv_plot(orig, bandpassed, Lx, Ly, inverse_fft=True
                       # latlon=area_extent
                       )
+    plt.savefig(save_path + '/sat_plot.png', dpi=300)
+    plt.show()
 
     # TODO check if this is mathematically the right way of calculating pspec
     pspec_2d = np.ma.masked_where(bandpassed.mask, abs(shifted_ft) ** 2)
     plot_2D_pspec(pspec_2d.data, Lx, Ly, wavelength_contours=[5, 10, 35])
+    plt.savefig(save_path + '/2d_pspec.png', dpi=300)
+    plt.show()
 
     wnum_bin_width = 0.1
     theta_bin_width = 5
@@ -271,5 +123,7 @@ if __name__ == '__main__':
     print(f'Dominant angle: {dominant_theta:.0f} deg from north')
 
     plot_radial_pspec(radial_pspec, wnum_vals, theta_bins, dominant_wnum)
+    plt.savefig(save_path + '/radial_pspec.png', dpi=300)
+    plt.show()
 
     print('smoothing?')
