e8f1d0d0131190c5e01c51388f0d94e6f0a68c36
HetteHoutman <h.houtman@hotmail.com>
2023-09-20 14:45:11 +0100

    init

--- run_all.py
+++ run_all.py
@@ -6,4 +6,4 @@ import pandas as pd
 df = pd.read_excel(sys.argv[1], header=1).reset_index(drop=True)
 
 for idx, row in df.iterrows():
-    os.system(f"python fourier_analysis.py ../tephi_plot/settings/{row.date.year}{row.date.month:02d}{row.date.day:02d}_{row.hour}.json {row.region}")
+    os.system(f"python fourier_analysis.py ../tephiplot/settings/{row.date.year}{row.date.month:02d}{row.date.day:02d}_{row.hour}.json {row.region}")
