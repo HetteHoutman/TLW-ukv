78bb5c1e1eebd3d6af7bc31b6e4ee8fbbe3e75ec
HetteHoutman <h.houtman@hotmail.com>
2023-10-24 17:23:15 +0100

    fn

--- plot_radsim.py
+++ plot_radsim.py
@@ -1,28 +1,15 @@
 import sys
 
-import netCDF4 as nc
 import matplotlib.pyplot as plt
-import iris
-import numpy as np
 
 from miscellaneous import check_argv_num
+from prepare_radsim_array import get_refl
 
 if __name__ == '__main__':
     check_argv_num(sys.argv, 1, '(radsim output file)')
     filename = sys.argv[1]
 
-    varname = 'refl'
-
-    output = nc.Dataset(filename)
-
-    if varname not in output.variables:
-        print('Error: varname ' + varname + ' not found in ' + filename)
-        sys.exit(1)
-    if 'lat' not in output.variables or 'lon' not in output.variables:
-        print('Error: netCDF file must contain lat and lon values')
-        sys.exit(1)
-
-    refl = output.variables['refl'][:].reshape(808, 621)[::-1]
+    refl = get_refl(filename)
     plt.imshow(refl, cmap='gray')
     plt.colorbar()
     plt.title('Reflectance')
