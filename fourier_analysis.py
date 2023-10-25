2d3800424380197d6c3000e44ab148256886ab1d
HetteHoutman <h.houtman@hotmail.com>
2023-10-25 14:47:57 +0100

    correct maximum finder and plot

--- fourier_analysis.py
+++ fourier_analysis.py
@@ -5,25 +5,32 @@ import iris.cube
 import iris.plot as iplt
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 import pandas as pd
 from astropy.convolution import convolve, Gaussian2DKernel
 from iris.analysis.cartography import rotate_winds
+import iris
 from matplotlib import colors
+from skimage.transform import rotate
 
-from cube_processing import read_variable, cube_at_single_level, add_orography, add_true_latlon_coords, \
-    create_latlon_cube
+from cube_processing import read_variable, cube_at_single_level, create_latlon_cube
 from fourier import *
 from fourier_plot import plot_pspec_polar, plot_radial_pspec, plot_2D_pspec, filtered_inv_plot, plot_corr
 from miscellaneous import check_argv_num, load_settings, get_region_var
 
 
-def plot_wind(w_cube, u_cube, v_cube, step=25, title='title'):
+def plot_wind(w_cube, u_cube, v_cube, empty, step=25, title='title'):
+
+    u_rot, v_rot = rotate_winds(u_cube, v_cube, empty.coord_system())
+    u_rot = u_rot.regrid(empty, iris.analysis.Linear())
+    v_rot = v_rot.regrid(empty, iris.analysis.Linear())
+
     fig, ax = plt.subplots(1, 1,
                            subplot_kw={'projection': ccrs.PlateCarree()}
                            )
     iplt.pcolormesh(w_cube[0], norm=mpl.colors.CenteredNorm())
-    iplt.quiver(u_cube[0, ::step, ::step], v_cube[0, ::step, ::step], pivot='middle')
+    iplt.quiver(u_rot[0, ::step, ::step], v_rot[0, ::step, ::step], pivot='middle')
     ax.gridlines(draw_labels=True)
     ax.coastlines()
     ax.set_xlabel('True Longitude / deg')
@@ -35,7 +42,7 @@ def plot_wind(w_cube, u_cube, v_cube, step=25, title='title'):
     # plt.xlim(lon_bounds)
     # plt.ylim(lat_bounds)
     plt.title(title)
-
+    plt.savefig(save_path + 'wind_plot.png', dpi=300)
 
 def get_datetime_from_settings(settings):
     return f'{settings.year}-{settings.month:02d}-{settings.day:02d}_{settings.h}'
@@ -78,8 +85,29 @@ def make_title_and_save_path(datetime, region, data_source_string, test, k2, smo
     return my_title, save_path
 
 
+def find_corr_max(collapsed_correlation, K, L, wavelengths, thetas):
+
+    halfway = K.shape[1] // 2
+    test_idx = np.nanargmax(collapsed_correlation)
+    dom_K, dom_L = K[:, halfway:].flatten()[test_idx], L[:, halfway:].flatten()[test_idx]
+    dominant_wlen, dominant_theta = wavelengths[:, halfway:].flatten()[test_idx], thetas[:, halfway:].flatten()[test_idx]
+
+    return dominant_wlen, dominant_theta, dom_K, dom_L
+
+
+def get_ellipse_correlation(pspec_2d, thetas, ell_shape):
+
+    corr = correlate_ellipse(pspec_2d, thetas, ell_shape)
+    rot_left_half = rotate(corr[:, :corr.shape[1] // 2 + 1], 180)
+    collapsed_corr = corr[:, corr.shape[1] // 2:] + rot_left_half
+
+    # mask bottom half of k_x = 0 line as this is the same as the top half
+    collapsed_corr.mask[collapsed_corr.shape[0] // 2:, 0] = True
+
+    return collapsed_corr
+
+
 if __name__ == '__main__':
-    # TODO clean and put in functions
     # TODO look for pp files automatically based on json file dates
 
     # options
@@ -87,38 +115,45 @@ if __name__ == '__main__':
     smoothed = True
     mag_filter = False
     test = True
+    use_sim_sat = False
+
+    min_lambda = 4
+    max_lambda = 30
+    wnum_bin_width = 0.05
+    theta_bin_width = 5
 
     # settings
     check_argv_num(sys.argv, 2, "(settings, region json files)")
     s = load_settings(sys.argv[1])
     datetime = get_datetime_from_settings(s)
     region = sys.argv[2]
-
     sat_bl, sat_tr, map_bl, map_tr = get_sat_map_bltr(region)
-
     my_title, save_path = make_title_and_save_path(datetime, region, 'ukv', test, k2, smoothed, mag_filter)
 
     # load data
-    w_cube = read_variable(s.file, 150, s.h)
-    u_cube = read_variable(s.file, 2, s.h).regrid(w_cube, iris.analysis.Linear())
-    v_cube = read_variable(s.file, 3, s.h).regrid(w_cube, iris.analysis.Linear())
+    empty_latlon = create_latlon_cube(sat_bl, sat_tr, n=501)
 
-    w_single_level, u_single_level, v_single_level = cube_at_single_level(s.map_height, w_cube, u_cube, v_cube,
-                                                                          bottomleft=map_bl, topright=map_tr)
+    # use pure ukv w-field
+    if not use_sim_sat:
+        w_cube = read_variable(s.file, 150, s.h)
+        u_cube = read_variable(s.file, 2, s.h).regrid(w_cube, iris.analysis.Linear())
+        v_cube = read_variable(s.file, 3, s.h).regrid(w_cube, iris.analysis.Linear())
 
-    empty = create_latlon_cube(sat_bl, sat_tr, n=501)
-    orig = w_single_level.regrid(empty, iris.analysis.Linear())
+        w_single_level, u_single_level, v_single_level = cube_at_single_level(s.map_height, w_cube, u_cube, v_cube,
+                                                                              bottomleft=map_bl, topright=map_tr)
 
-    # plot wind comparison
-    u_rot, v_rot = rotate_winds(u_single_level, v_single_level, empty.coord_system())
-    u_rot = u_rot.regrid(empty, iris.analysis.Linear())
-    v_rot = v_rot.regrid(empty, iris.analysis.Linear())
-    plot_wind(orig, u_rot, v_rot, title=my_title)
-    plt.savefig(save_path + 'wind_plot.png', dpi=300)
+        orig = w_single_level.regrid(empty_latlon, iris.analysis.Linear())
 
-    # prepare data for fourier analysis
-    Lx, Ly = extract_distances(orig.coords('latitude')[0].points, orig.coords('longitude')[0].points)
-    orig = orig[0, ::-1].data
+        # plot wind comparison
+        plot_wind(orig, u_single_level, v_single_level, empty_latlon, title=my_title)
+
+        # prepare data for fourier analysis
+        Lx, Ly = extract_distances(orig.coords('latitude')[0].points, orig.coords('longitude')[0].points)
+        orig = orig[0, ::-1].data
+
+    # else use simulated satellite imagery
+    else:
+        pass
 
     # get reciprocal space coordinates
     K, L, wavenumbers, thetas = recip_space(Lx, Ly, orig.shape)
@@ -131,8 +166,6 @@ if __name__ == '__main__':
     shifted_ft = np.fft.fftshift(ft)
 
     # bandpass through expected TLW wavelengths
-    min_lambda = 4
-    max_lambda = 30
     bandpassed = ideal_bandpass(shifted_ft, Lx, Ly, 2 * np.pi / max_lambda, 2 * np.pi / min_lambda)
 
     # plot ingoing data
@@ -149,8 +182,6 @@ if __name__ == '__main__':
         pspec_2d = np.ma.masked_where(pspec_2d.mask, pspec_2d.data * wavenumbers ** 2)
 
     # convert power spectrum to polar coordinates
-    wnum_bin_width = 0.05
-    theta_bin_width = 5
     # noinspection PyTupleAssignmentBalance
     radial_pspec, wnum_bins, wnum_vals, theta_bins, theta_vals = make_polar_pspec(pspec_2d, wavenumbers, wnum_bin_width,
                                                                                   thetas, theta_bin_width)
@@ -159,11 +190,6 @@ if __name__ == '__main__':
         pspec_2d = np.ma.masked_where(pspec_2d.mask, convolve(pspec_2d.data, Gaussian2DKernel(7, x_size=15, y_size=15),
                                                               boundary='wrap'))
         radial_pspec = convolve(radial_pspec, Gaussian2DKernel(3, x_size=11, y_size=11), boundary='wrap')
-
-    # plot (smoothed) cartesian power spectrum
-    plt.figure()
-    plot_2D_pspec(pspec_2d, Lx, Ly, wavelength_contours=[5, 10, 35], title=my_title)
-    plt.savefig(save_path + '2d_pspec.png', dpi=300)
 
     # find maximum in polar power spectrum
     bounded_polar_pspec, bounded_wnum_vals = apply_wnum_bounds(radial_pspec, wnum_vals, wnum_bins,
@@ -186,38 +212,31 @@ if __name__ == '__main__':
     plt.figure()
     plot_radial_pspec(radial_pspec, wnum_vals, theta_bins, dominant_wnum, title=my_title)
     plt.savefig(save_path + 'radial_pspec.png', dpi=300)
-    # plt.show()
 
     # perform correlation with ellipse
-    corr = correlate_ellipse(pspec_2d, thetas, (2, 25))
-    rot_left_half = rotate(corr[:, :corr.shape[1] // 2 + 1], 180)
-    collapsed_corr = corr[:, corr.shape[1] // 2:] + rot_left_half
-
-    # mask bottom half of k_x = 0 line as this is the same as the top half
-    collapsed_corr.mask[collapsed_corr.shape[0] // 2:, 0] = True
+    collapsed_corr = get_ellipse_correlation(pspec_2d, thetas, (2, 25))
 
     # find maximum in correlation array
-    idxs = np.unravel_index(collapsed_corr.argmax(), collapsed_corr.shape)
-    dom_K, dom_L = K[idxs[0], K.shape[1] // 2 + idxs[1]], L[::-1][idxs[0], L.shape[1] // 2 + idxs[1]]
-    dominant_wlen = wavelengths[:, wavelengths.shape[1] // 2:][idxs[0], idxs[1]]
-    dominant_theta = thetas[:, thetas.shape[1] // 2:][idxs[0], idxs[1]]
+    dominant_wlen, dominant_theta, dom_K, dom_L = find_corr_max(collapsed_corr, K, L, wavelengths, thetas)
 
     # plot correlation array with maximum
-    # TODO dom scatter not right
     plt.figure()
-    plot_corr(collapsed_corr, L, K)
+    plot_corr(collapsed_corr, K, L)
     plt.scatter(dom_K, dom_L, marker='x')
     plt.savefig(save_path + 'corr.png', dpi=300)
 
-    # plot 2d power spectrum with maximum from ellipse correlation
+    # plot cartesian power spectrum with maximum from ellipse correlation
     plt.figure()
-    plot_2D_pspec(pspec_2d, Lx, Ly, wavelength_contours=[5, 10, 35], title=my_title)
+    plot_2D_pspec(pspec_2d, K, L, wavelengths, wavelength_contours=[5, 10, 35], title=my_title)
     plt.scatter(dom_K, dom_L, marker='x')
+    plt.xlim(-2,2)
+    plt.ylim(-2,2)
     plt.savefig(save_path + '2d_pspec_withcross.png', dpi=300)
 
     print(f'Dominant wavelength by ellipse method: {dominant_wlen:.2f} km')
     print(f'Dominant angle by ellipse method: {dominant_theta:.0f} deg from north')
 
+    # save to csv with results
     if not test:
         df = pd.read_excel('../sat_vs_ukv_results.xlsx', index_col=[0, 1])
         df.loc[(f'{s.year}-{s.month:02d}-{s.day:02d}', region), 'ukv_lambda_ellipse'] = dominant_wlen
