5ebc17b692ce88f6fdccb6486d01bda41a9f7864
HetteHoutman <h.houtman@hotmail.com>
2023-10-27 15:54:31 +0100

    better datetime

--- run_all.py
+++ run_all.py
@@ -6,4 +6,5 @@ import pandas as pd
 df = pd.read_excel(sys.argv[1], index_col=[0,1])
 
 for idx, row in df.iterrows():
-    os.system(f"python fourier_analysis.py ../tephiplot/settings/{idx[0][:4]}{idx[0][5:7]}{idx[0][8:10]}_{row.hour}.json {idx[1]}")
+    datetime_str = str(idx[0][:4]) + str(idx[0][5:7]) + str(idx[0][8:10]) + '_' + str(row.hour)
+    os.system(f"python fourier_analysis.py ../tephiplot/settings/{datetime_str}.json {idx[1]}")
